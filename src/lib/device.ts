14314a7b44c2d8e9275b94c47ba33d9ab4e773cd
ryrun <ryrun@online.de>
2025-03-25 20:30:21 +0100

    Add semicolons to match project style, Zed autoformatting.

--- src/lib/device.ts
+++ src/lib/device.ts
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright (C) 2023, Input Labs Oy.
 
-import { AsyncSubject } from 'rxjs'
-import { HID } from 'lib/hid'
-import { timeoutPromise } from 'lib/delay'
-import { Profiles } from 'lib/profiles'
-import { delay } from 'lib/delay'
-import { Tunes, PresetWithValues } from 'lib/tunes'
+import { AsyncSubject } from "rxjs";
+import { HID } from "lib/hid";
+import { timeoutPromise } from "lib/delay";
+import { Profiles } from "lib/profiles";
+import { delay } from "lib/delay";
+import { Tunes, PresetWithValues } from "lib/tunes";
 import {
   Ctrl,
   CtrlProtocolFlags,
@@ -25,166 +25,168 @@ import {
   CtrlStatusSet,
   CtrlStatusShare,
   CtrlProfileOverwrite,
-} from 'lib/ctrl'
+} from "lib/ctrl";
 
-const ADDR_IN = 3
-const ADDR_OUT = 4
-const TIMEOUT = 500
+const ADDR_IN = 3;
+const ADDR_OUT = 4;
+const TIMEOUT = 500;
 
 export class Device {
-  usbDevice: USBDevice
-  proxiedDevice?: Device
-  proxyEnabled: boolean = false
-  deviceVersion = [0, 0, 0]
-  logs: string[] = []
-  logsProxy: string[] = []
-  isConnected = false
-  isConnectedRaw = false
-  isListening = false
-  isBusy = false
-  failed = false
-  failedError?: Error
-  pendingConfig?: AsyncSubject<CtrlConfigShare>
-  pendingProfile?: AsyncSubject<CtrlSection>
-  profiles: Profiles
-  tunes: Tunes
+  usbDevice: USBDevice;
+  proxiedDevice?: Device;
+  proxyEnabled: boolean = false;
+  deviceVersion = [0, 0, 0];
+  logs: string[] = [];
+  logsProxy: string[] = [];
+  isConnected = false;
+  isConnectedRaw = false;
+  isListening = false;
+  isBusy = false;
+  failed = false;
+  failedError?: Error;
+  pendingConfig?: AsyncSubject<CtrlConfigShare>;
+  pendingProfile?: AsyncSubject<CtrlSection>;
+  profiles: Profiles;
+  tunes: Tunes;
 
   constructor(usbDevice: USBDevice) {
-    this.usbDevice = usbDevice
-    this.openDevice()
+    this.usbDevice = usbDevice;
+    this.openDevice();
     // (<any>window).device = this.device
-    this.profiles = new Profiles(this)
-    this.tunes = new Tunes(this)
+    this.profiles = new Profiles(this);
+    this.tunes = new Tunes(this);
   }
 
   disconnectCallback() {
-    this.logs = []
-    this.isConnected = false
-    this.isConnectedRaw = false
-    this.isListening = false
-    this.deviceVersion = [0, 0, 0]
+    this.logs = [];
+    this.isConnected = false;
+    this.isConnectedRaw = false;
+    this.isListening = false;
+    this.deviceVersion = [0, 0, 0];
   }
 
   async openDevice() {
     try {
       this.failed = false;
-      await this.usbDevice.open()
-      console.log('Device opened')
-      await this.usbDevice.selectConfiguration(1)
-      console.log('Configuration selected')
-      await this.usbDevice.claimInterface(1)
-      console.log('Interface claimed')
-      await this.sendEmpty()
+      await this.usbDevice.open();
+      console.log("Device opened");
+      await this.usbDevice.selectConfiguration(1);
+      console.log("Configuration selected");
+      await this.usbDevice.claimInterface(1);
+      console.log("Interface claimed");
+      await this.sendEmpty();
       this.isConnected = true;
       this.isConnectedRaw = true;
-      await this.sendStatusGet()
+      await this.sendStatusGet();
     } catch (error) {
-      this.failed = true
-      this.failedError = error as Error
-      throw error
+      this.failed = true;
+      this.failedError = error as Error;
+      throw error;
     }
-    this.listen()
+    this.listen();
   }
 
   async listen() {
-    this.isListening = true
+    this.isListening = true;
     try {
-      const response = await this.usbDevice.transferIn(ADDR_IN, PACKAGE_SIZE)
-      let data = response.data as any
-      const array = new Uint8Array(data.buffer)
-      const ctrl = Ctrl.decode(array)
+      const response = await this.usbDevice.transferIn(ADDR_IN, PACKAGE_SIZE);
+      let data = response.data as any;
+      const array = new Uint8Array(data.buffer);
+      const ctrl = Ctrl.decode(array);
       // console.log('Received', ctrl)
-      if (ctrl instanceof CtrlLog) this.handleCtrlLog(ctrl)
-      if (ctrl instanceof CtrlStatusShare) this.handleCtrlStatusShare(ctrl)
+      if (ctrl instanceof CtrlLog) this.handleCtrlLog(ctrl);
+      if (ctrl instanceof CtrlStatusShare) this.handleCtrlStatusShare(ctrl);
       if (ctrl instanceof CtrlConfigShare) {
         // console.log(ctrl)
         if (this.pendingConfig) {
-          this.pendingConfig.next(ctrl)
-          this.pendingConfig.complete()
-          this.pendingConfig = undefined
+          this.pendingConfig.next(ctrl);
+          this.pendingConfig.complete();
+          this.pendingConfig = undefined;
         } else {
-          this.handleCtrlConfigShare(ctrl)
+          this.handleCtrlConfigShare(ctrl);
         }
       }
       if (ctrl instanceof CtrlSection) {
         // console.log(ctrl)
         if (this.pendingProfile) {
-          this.pendingProfile.next(ctrl as CtrlSection)
-          this.pendingProfile.complete()
-          this.pendingProfile = undefined
+          this.pendingProfile.next(ctrl as CtrlSection);
+          this.pendingProfile.complete();
+          this.pendingProfile = undefined;
         }
       }
-    } catch (error:any) {
-      console.warn(error)
-      return
+    } catch (error: any) {
+      console.warn(error);
+      return;
     }
-    await this.listen()
+    await this.listen();
   }
 
   async waitUntilReady() {
-    let attempts = 0
+    let attempts = 0;
     while (!this.isListening || this.isBusy) {
-      await delay(100)
-      attempts += 1
+      await delay(100);
+      attempts += 1;
       if (attempts > 10) {
         if (this.isBusy) {
-          throw Error('Device timeout (busy)')
+          throw Error("Device timeout (busy)");
         }
-        break
+        break;
       }
     }
   }
 
   getName() {
-    return this.usbDevice.productName
+    return this.usbDevice.productName;
   }
 
   getConnectorName() {
-    if (this.proxyEnabled) return this.usbDevice.productName  // Proxy is bypassed.
-    else return this.getName()
+    if (this.proxyEnabled)
+      return this.usbDevice.productName; // Proxy is bypassed.
+    else return this.getName();
   }
 
   isController() {
-    if (this.usbDevice.productName == 'Alpakka') return true
-    return false
+    if (this.usbDevice.productName == "Alpakka") return true;
+    return false;
   }
 
   isDongle() {
-    return !this.isController()
+    return !this.isController();
   }
 
   isAlpakkaV0() {
-    return this.usbDevice.serialNumber == 'v0'
+    return this.usbDevice.serialNumber == "v0";
   }
 
   isAlpakkaV1() {
-    return this.usbDevice.serialNumber == 'v1'
+    return this.usbDevice.serialNumber == "v1";
   }
 
   isProxy() {
-    return false
+    return false;
   }
 
   clearLogs() {
-    if (this.proxyEnabled) this.logsProxy = []
-    else this.logs = []
+    if (this.proxyEnabled) this.logsProxy = [];
+    else this.logs = [];
   }
 
   handleCtrlLog(ctrl: CtrlLog) {
-    let targetLogs = this.logs
-    if (ctrl.protocolFlags == CtrlProtocolFlags.WIRELESS) targetLogs = this.logsProxy
-    if (!targetLogs[0] || targetLogs[0]?.endsWith('\n')) {
-      targetLogs.unshift(ctrl.logMessage)
+    let targetLogs = this.logs;
+    if (ctrl.protocolFlags == CtrlProtocolFlags.WIRELESS)
+      targetLogs = this.logsProxy;
+    if (!targetLogs[0] || targetLogs[0]?.endsWith("\n")) {
+      targetLogs.unshift(ctrl.logMessage);
     } else {
-      targetLogs[0] += ctrl.logMessage
+      targetLogs[0] += ctrl.logMessage;
     }
     // console.log(ctrl.logMessage)
   }
 
   handleCtrlStatusShare(ctrl: CtrlStatusShare) {
-    this.deviceVersion = ctrl.version
-    console.log('Firmware of connected device:', this.deviceVersion)
-    this.sendStatusSet()
+    this.deviceVersion = ctrl.version;
+    console.log("Firmware of connected device:", this.deviceVersion);
+    this.sendStatusSet();
   }
 
   handleCtrlConfigShare(ctrl: CtrlConfigShare) {
@@ -192,134 +194,155 @@ export class Device {
     // change made on the controller via shortcuts.
     // TODO: Investigate why the ctrl object does not return real data that
     // could be used directly (instead of nuking all data). Firmware bug?
-    this.tunes.invalidatePresets()
+    this.tunes.invalidatePresets();
   }
 
   async sendEmpty() {
-    const data = new Uint8Array(64)
-    await this.usbDevice.transferOut(ADDR_OUT, data)
+    const data = new Uint8Array(64);
+    await this.usbDevice.transferOut(ADDR_OUT, data);
   }
 
   async sendStatusGet() {
-    const data = new CtrlStatusGet()
-    await this.send(data)
+    const data = new CtrlStatusGet();
+    await this.send(data);
   }
 
   async sendStatusSet() {
-    const data = new CtrlStatusSet(Date.now())
-    await this.send(data)
+    const data = new CtrlStatusSet(Date.now());
+    await this.send(data);
   }
 
   async sendProc(proc: HID) {
-    const data = new CtrlProc(proc)
-    await this.send(data)
+    const data = new CtrlProc(proc);
+    await this.send(data);
   }
 
   async sendProfileOverwrite(indexTo: number, indexFrom: number) {
-    const data = new CtrlProfileOverwrite(indexTo, indexFrom)
-    await this.send(data)
+    const data = new CtrlProfileOverwrite(indexTo, indexFrom);
+    await this.send(data);
   }
 
-  async send(ctrl: CtrlProc | CtrlStatusGet | CtrlStatusSet | CtrlConfigGet | CtrlProfileGet) {
+  async send(
+    ctrl:
+      | CtrlProc
+      | CtrlStatusGet
+      | CtrlStatusSet
+      | CtrlConfigGet
+      | CtrlProfileGet,
+  ) {
     if (this.proxyEnabled) {
-      ctrl.protocolFlags = CtrlProtocolFlags.WIRELESS
+      ctrl.protocolFlags = CtrlProtocolFlags.WIRELESS;
     }
     // console.log(ctrl)
-    await this.usbDevice.transferOut(ADDR_OUT, ctrl.encode())
+    await this.usbDevice.transferOut(ADDR_OUT, ctrl.encode());
   }
 
   async getConfig(index: ConfigIndex): Promise<PresetWithValues> {
-    this.pendingConfig = new AsyncSubject()
-    const ctrlOut = new CtrlConfigGet(index)
-    await this.send(ctrlOut)
-    const responsePromise: Promise<PresetWithValues> = new Promise((resolve, reject) => {
-      this.pendingConfig?.subscribe({
-        next: (ctrlIn) => {
-          resolve({presetIndex: ctrlIn.preset, values: ctrlIn.values})
-        }
-      })
-    })
-    const timeoutMessage = `Timeout in getConfig ${ConfigIndex[index]}`
-    const timeout = timeoutPromise(TIMEOUT, timeoutMessage) as Promise<PresetWithValues>
-    return Promise.race([responsePromise, timeout])
+    this.pendingConfig = new AsyncSubject();
+    const ctrlOut = new CtrlConfigGet(index);
+    await this.send(ctrlOut);
+    const responsePromise: Promise<PresetWithValues> = new Promise(
+      (resolve, reject) => {
+        this.pendingConfig?.subscribe({
+          next: (ctrlIn) => {
+            resolve({ presetIndex: ctrlIn.preset, values: ctrlIn.values });
+          },
+        });
+      },
+    );
+    const timeoutMessage = `Timeout in getConfig ${ConfigIndex[index]}`;
+    const timeout = timeoutPromise(
+      TIMEOUT,
+      timeoutMessage,
+    ) as Promise<PresetWithValues>;
+    return Promise.race([responsePromise, timeout]);
   }
 
-  async setConfig(index: ConfigIndex, preset: number, values: number[]): Promise<number> {
-    this.pendingConfig = new AsyncSubject()
-    const ctrlOut = new CtrlConfigSet(index, preset, values)
-    await this.send(ctrlOut)
+  async setConfig(
+    index: ConfigIndex,
+    preset: number,
+    values: number[],
+  ): Promise<number> {
+    this.pendingConfig = new AsyncSubject();
+    const ctrlOut = new CtrlConfigSet(index, preset, values);
+    await this.send(ctrlOut);
     const responsePromise: Promise<number> = new Promise((resolve, reject) => {
       this.pendingConfig?.subscribe({
         next: (ctrlIn) => {
-          resolve(ctrlIn.preset)
-        }
-      })
-    })
-    const timeoutMessage = `Timeout in setConfig ${ConfigIndex[index]}`
-    const timeout = timeoutPromise(TIMEOUT, timeoutMessage) as Promise<number>
-    return Promise.race([responsePromise, timeout])
+          resolve(ctrlIn.preset);
+        },
+      });
+    });
+    const timeoutMessage = `Timeout in setConfig ${ConfigIndex[index]}`;
+    const timeout = timeoutPromise(TIMEOUT, timeoutMessage) as Promise<number>;
+    return Promise.race([responsePromise, timeout]);
   }
 
   async getSection(
     profileIndex: number,
     sectionIndex: SectionIndex,
   ): Promise<CtrlSection> {
-    await this.waitUntilReady()
-    this.isBusy = true
-    this.pendingProfile = new AsyncSubject()
-    const ctrlOut = new CtrlProfileGet(profileIndex, sectionIndex)
+    await this.waitUntilReady();
+    this.isBusy = true;
+    this.pendingProfile = new AsyncSubject();
+    const ctrlOut = new CtrlProfileGet(profileIndex, sectionIndex);
     await this.send(ctrlOut).catch((error) => {
-      this.isBusy = false
-      throw error
-    })
-    const responsePromise: Promise<CtrlSection> = new Promise((resolve, reject) => {
-      this.pendingProfile?.subscribe({
-        next: (ctrlIn) => {
-          resolve(ctrlIn)
-        }
-      })
-    })
-    const timeoutMessage = `Timeout in getSection ${SectionIndex[sectionIndex]}`
-    const timeout = timeoutPromise(TIMEOUT, timeoutMessage) as Promise<CtrlSection>
+      this.isBusy = false;
+      throw error;
+    });
+    const responsePromise: Promise<CtrlSection> = new Promise(
+      (resolve, reject) => {
+        this.pendingProfile?.subscribe({
+          next: (ctrlIn) => {
+            resolve(ctrlIn);
+          },
+        });
+      },
+    );
+    const timeoutMessage = `Timeout in getSection ${SectionIndex[sectionIndex]}`;
+    const timeout = timeoutPromise(
+      TIMEOUT,
+      timeoutMessage,
+    ) as Promise<CtrlSection>;
     return Promise.race([responsePromise, timeout]).finally(() => {
-      this.isBusy = false
-    })
+      this.isBusy = false;
+    });
   }
 
-  async setSection(
-    profileIndex: number,
-    section: CtrlSection,
-  ) {
-    this.pendingProfile = new AsyncSubject()
-    const ctrlOut = new CtrlProfileSet(profileIndex, section.sectionIndex, section.payload())
-    await this.send(ctrlOut)
+  async setSection(profileIndex: number, section: CtrlSection) {
+    this.pendingProfile = new AsyncSubject();
+    const ctrlOut = new CtrlProfileSet(
+      profileIndex,
+      section.sectionIndex,
+      section.payload(),
+    );
+    await this.send(ctrlOut);
     const responsePromise = new Promise((resolve, reject) => {
       this.pendingProfile?.subscribe({
         next: (ctrlIn) => {
-          resolve(ctrlIn)
-        }
-      })
-    })
-    const timeoutMessage = `Timeout in setSection`
-    const timeout = timeoutPromise(TIMEOUT, timeoutMessage)
-    return Promise.race([responsePromise, timeout])
+          resolve(ctrlIn);
+        },
+      });
+    });
+    const timeoutMessage = `Timeout in setSection`;
+    const timeout = timeoutPromise(TIMEOUT, timeoutMessage);
+    return Promise.race([responsePromise, timeout]);
   }
-
 }
 
 // Fake wireless device connected to dongle.
 // The WebUSB underlying operations are controlled by the dongle device,
 // but some properties are overridden via proxy.
 export const deviceWirelessProxyHandler = {
-  get(target:Device, property:keyof Device) {
-    const key = String(property)
-    if (key == 'getName') return ()=>'Alpakka'
-    if (key == 'logs') return target.logsProxy
-    if (key == 'isController') return ()=>true
-    if (key == 'isDongle') return ()=>false
-    if (key == 'isAlpakkaV0') return ()=>false
-    if (key == 'isAlpakkaV1') return ()=>true
-    if (key == 'isProxy') return ()=>true
-    return target[property]
-  }
-}
+  get(target: Device, property: keyof Device) {
+    const key = String(property);
+    if (key == "getName") return () => "Alpakka";
+    if (key == "logs") return target.logsProxy;
+    if (key == "isController") return () => true;
+    if (key == "isDongle") return () => false;
+    if (key == "isAlpakkaV0") return () => false;
+    if (key == "isAlpakkaV1") return () => true;
+    if (key == "isProxy") return () => true;
+    return target[property];
+  },
+};
